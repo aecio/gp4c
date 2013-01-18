1a5b02d34909865cc8425dcb74c9182c37000de8
Aecio Santos <aecio.solando@gmail.com,aeciosantos@dcc.ufmg.br>
2013-01-18 12:48:28 -0200

    Change in gp selector output file.

--- src/gp_selector.h
+++ src/gp_selector.h
@@ -101,7 +101,7 @@ public:
             top_out << "evolution:" << gp->fitness_e <<
                        " validation:" << gp->fitness_v <<
                        " cycles_std_dev:" << gp->cycles_std_dev <<
-                       " fitness_std_dev: "<< gp->fitness_std_dev <<
+                       " fitness_std_dev:"<< gp->fitness_std_dev <<
                        " sum_score:" << sum_score <<
                        " avg_score:" << avg_score <<
                        " " << *gp;
